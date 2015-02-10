d46cb21f6d9878af1ca11eca5357c1667cbdfe11
tasawernawaz <tasawer.nawaz@arbisoft.com>
2015-02-10 20:04:16 +0500

    end of lesson 1 of js

---
+++ js/resumeBuilder.js
@@ -0,0 +1,107 @@
+var bio = {
+    'Name': "Tasawer Nawaz",
+    'Role': "Web Developer",
+    'contacts': {
+        "Phone": "03337214307",
+        "Email": "tasawernawaz@gmail.com",
+        "Github": "tasawernawaz",
+        'Location': "Lahore, Pakistan"
+    },
+
+    'Image_url': "images/fry.jpg",
+    'welcomeMessage': 'Welcome to my resume',
+    'Skills': ['Python', 'Scrapy', 'Javascript', 'CSS']
+};
+
+var work = {
+    'jobs': [{
+        'title': "Software Engineer",
+        'employer': "Arbisoft",
+        'dates': "2012-2015",
+        'location': "Lahore",
+        'description': 'Currently I am software engineer at arbisot since december 2012 working on python, scrapy, djnago and rest api'
+    }]
+};
+
+var education = {
+    'schools': [
+        {
+            'name': 'University of Gujrat',
+            'degree': 'BS CS',
+            'dates': "2008-2012",
+            'location': 'Gujrat',
+            'major': ['Computer Science'],
+            'url': 'www.uog.edu.pk/'
+        },
+        {
+            'name': 'F.G Degree Collage Kharian Cant',
+            'degree': 'ICS',
+            'dates': "2006-2008",
+            'location': 'Kharian',
+            'major': ['Computer Science', 'Physics', 'Math'],
+            'url': 'https://www.facebook.com/fg.degree.college'
+        }
+    ],
+    'onlineCourses ': [
+        {
+            'title': 'Udacity Nanodegree',
+            'school': 'Udacity',
+            'dates': "2015 to current",
+            'url': 'udacity.com/course/nd001'
+        }
+    ]
+};
+
+var projects = {
+    'projects': [
+        {
+            'title': 'aee',
+            'description': 'mostly work done ia related to python-scrapy',
+            'dates': "2012-2014",
+            'images': ['images/aee.png']
+        }
+    ]
+};
+
+
+//
+//var header = $("#header");
+//var myName = HTMLheaderName.replace("%data%", bio.Name);
+//var myRole = HTMLheaderRole.replace("%data%", bio.Role);
+//header.prepend(myRole);
+//header.prepend(myName);
+
+//    header.append(HTMLbioPic.replace("%data%", bio.Image_url));
+//    header.append(HTMLWelcomeMsg.replace("%data%", bio.Welcome_message));
+
+//var contact = $("#topContacts");
+//contact.append(HTMLmobile.replace("%data%", bio.Contact_Info.Phone));
+//contact.append(HTMLemail.replace("%data%", bio.Contact_Info.Email));
+//contact.append(HTMLgithub.replace("%data%", bio.Contact_Info.Github));
+//contact.append(HTMLlocation.replace("%data%", bio.Contact_Info.Location));
+
+
+//    var skills =HTMLskills;
+//    skills.append(HTMLskills.replace("%data%", skills[0]));
+
+//$("#workExperience").append(HTMLworkStart);
+//var work_div = $(".work-entry");
+//work_div.append(HTMLworkTitle.replace("%data%", work.current_position));
+//work_div.append(HTMLworkEmployer.replace("%data%", work.employer));
+//work_div.append(HTMLworkDates.replace("%data%", work.years_worked));
+//work_div.append(HTMLworkLocation.replace("%data%", work.city));
+
+
+//
+//$("#education").append(HTMLschoolStart);
+//var edu_div = $(".education-entry");
+//edu_div.append(HTMLschoolName.replace("%data%", education.last_school));
+//edu_div.append(HTMLschoolDates.replace("%data%", education.year));
+//edu_div.append(HTMLschoolLocation.replace("%data%", education.city));
+
+//
+//        var work_div = $("#workExperience");
+//    work_div.append(HTMLworkTitle.replace("%data%", work.current_position));
+//    work_div.append(HTMLworkEmployer.replace("%data%", work.employer));
+//    work_div.append(HTMLworkDates.replace("%data%", work.years_worked));
+//    work_div.append(HTMLworkLocation.replace("%data%", work.city));
